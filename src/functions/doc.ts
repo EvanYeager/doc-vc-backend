501f6db1433b248624cc172c2fae1187bbac6880
evan-yeager <eyeager1@umd.edu>
2026-04-05 12:41:50 -0400

    add get document metadata

--- src/functions/doc.ts
+++ src/functions/doc.ts
@@ -1,40 +1,41 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { getPool } from "../db";
 
-export async function docs(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function doc(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
-    const username = request.query.get("username") || "";
 
     try {
         const sql = require('mssql');
         const pool = await getPool();
         context.log('SQL connected successfully');
-        
+
         const result = await pool.request()
-            .input('username', sql.VarChar(50), username)
-            .query("select a.user_id, assignment_id, a.title, a.description, a.created_at from assignments a, users b where a.user_id = b.user_id and b.username = @username");
-        const rows = result.recordset;
+            .input('id', sql.Int, request.params.id)
+            .query("select a.user_id, assignment_id, a.title, a.description, a.created_at from assignments a where a.assignment_id = @id");
+        const row = result.recordset[0];
 
         return {
             status: 200,
             body: JSON.stringify({
-                result: rows
+                result: row
             }),
             headers: { 'Content-Type': 'application/json' }
-        };
+        }
     } catch (err) {
         context.log(`SQL connection failed: ${(err as Error).message}`);
         return {
             status: 500,
-            body: JSON.stringify({ error: err.message }),
+            body: JSON.stringify({ error: (err as Error).message }),
             headers: { 'Content-Type': 'application/json' }
         };
     }
+
+    return { body: `Hello, ${name}!` };
 };
 
-app.http('docs', {
-    route: 'meta/docs',
+app.http('doc', {
+    route: 'meta/doc/{id}',
     methods: ['GET'],
     authLevel: 'anonymous',
-    handler: docs
+    handler: doc
 });
